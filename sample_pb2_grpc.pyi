51b01c9800a0ef2fa1116ff25eb8856f135ef711
ijknabla <ijknabla@gmail.com>
2024-01-28 10:14:16 +0900

    Separate property

--- sample_pb2_grpc.pyi
+++ sample_pb2_grpc.pyi
@@ -3,23 +3,28 @@ from google.protobuf import empty_pb2 as google_dot_protobuf_dot_empty__pb2
 
 import grpc_pyi_plugin as _plugin
 
+_UU = _plugin.UnaryUnaryProperty[
+    google_dot_protobuf_dot_empty__pb2.Empty,
+    google_dot_protobuf_dot_empty__pb2.Empty,
+]
+_US = _plugin.UnaryStreamProperty[
+    google_dot_protobuf_dot_empty__pb2.Empty,
+    google_dot_protobuf_dot_empty__pb2.Empty,
+]
+_SU = _plugin.StreamUnaryProperty[
+    google_dot_protobuf_dot_empty__pb2.Empty,
+    google_dot_protobuf_dot_empty__pb2.Empty,
+]
+_SS = _plugin.StreamStreamProperty[
+    google_dot_protobuf_dot_empty__pb2.Empty,
+    google_dot_protobuf_dot_empty__pb2.Empty,
+]
+
 class SampleStub(_plugin.GenericStub[_plugin.ChannelType]):
-    UU: _plugin.UnaryUnaryProperty[
-        google_dot_protobuf_dot_empty__pb2.Empty,
-        google_dot_protobuf_dot_empty__pb2.Empty,
-    ]
-    US: _plugin.UnaryStreamProperty[
-        google_dot_protobuf_dot_empty__pb2.Empty,
-        google_dot_protobuf_dot_empty__pb2.Empty,
-    ]
-    SU: _plugin.StreamUnaryProperty[
-        google_dot_protobuf_dot_empty__pb2.Empty,
-        google_dot_protobuf_dot_empty__pb2.Empty,
-    ]
-    SS: _plugin.StreamStreamProperty[
-        google_dot_protobuf_dot_empty__pb2.Empty,
-        google_dot_protobuf_dot_empty__pb2.Empty,
-    ]
+    UU: _UU
+    US: _US
+    SU: _SU
+    SS: _SS
 
 class SampleServicer(_plugin.GenericServicer[_plugin.ServerType]): ...
 
