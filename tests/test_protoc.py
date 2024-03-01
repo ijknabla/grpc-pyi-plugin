74d4e80985d7e8fcdfe49fc2b786e387c95e0f7e
ijknabla <ijknabla@gmail.com>
2024-03-01 17:18:30 +0900

    Add source file

--- tests/test_protoc.py
+++ tests/test_protoc.py
@@ -4,16 +4,26 @@ import sys
 from asyncio.subprocess import Process, create_subprocess_exec
 from collections.abc import AsyncIterator, Awaitable
 from contextlib import AsyncExitStack, asynccontextmanager
+from pathlib import Path
 
 import pytest
 
 
 @pytest.mark.asyncio
-async def test_protoc() -> None:
+async def test_protoc(
+    sample_proto_path: Path,
+) -> None:
     async with AsyncExitStack() as stack:
         aenter = stack.enter_async_context
         protoc = await aenter(
-            terminating(create_subprocess_exec(sys.executable, "-mgrpc_tools.protoc"))
+            terminating(
+                create_subprocess_exec(
+                    sys.executable,
+                    "-mgrpc_tools.protoc",
+                    f"-I{sample_proto_path.parent}",
+                    f"{sample_proto_path.name}",
+                )
+            )
         )
         assert (await protoc.wait()) == 0
 
