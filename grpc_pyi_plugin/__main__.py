92ba937255732ca91216cd59e688a03e2412e560
ijknabla <ijknabla@gmail.com>
2024-03-01 17:27:48 +0900

    Deserialize script

--- grpc_pyi_plugin/__main__.py
+++ grpc_pyi_plugin/__main__.py
@@ -1,4 +1,11 @@
-def main() -> None: ...
+import sys
+
+from google.protobuf.compiler.plugin_pb2 import CodeGeneratorRequest
+
+
+def main() -> None:
+    request = CodeGeneratorRequest.FromString(sys.stdin.buffer.read())
+    print(request, file=sys.stderr)
 
 
 if __name__ == "__main__":
