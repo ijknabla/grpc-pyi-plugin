1163e2a8d203778f78a2d7ccf9cd24ae1c729647
ijknabla <ijknabla@gmail.com>
2024-03-01 18:49:39 +0900

    Output file

--- grpc_pyi_plugin/__main__.py
+++ grpc_pyi_plugin/__main__.py
@@ -1,11 +1,33 @@
-import sys
+from __future__ import annotations
 
-from google.protobuf.compiler.plugin_pb2 import CodeGeneratorRequest
+import sys
+from collections.abc import Sequence
+from pathlib import Path
+from typing import TYPE_CHECKING
+
+from google.protobuf.compiler.plugin_pb2 import CodeGeneratorResponse
+from google.protobuf.descriptor_pb2 import FileDescriptorProto
+from google.protobuf.message import Message
+
+if not TYPE_CHECKING:
+    from google.protobuf.compiler.plugin_pb2 import CodeGeneratorRequest
+else:
+
+    class CodeGeneratorRequest(Message):
+        source_file_descriptors: Sequence[FileDescriptorProto]
 
 
 def main() -> None:
     request = CodeGeneratorRequest.FromString(sys.stdin.buffer.read())
     print(request, file=sys.stderr)
+
+    response = CodeGeneratorResponse()
+
+    for descriptor in request.source_file_descriptors:
+        source = Path(descriptor.name)
+        response.file.append(CodeGeneratorResponse.File(name=f"{source.stem}_pb2_grpc.pyi"))
+
+    sys.stdout.buffer.write(response.SerializeToString())
 
 
 if __name__ == "__main__":
